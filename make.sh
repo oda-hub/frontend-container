ede7199e6be919607a3c06806d5fd9939e4e4baa
Volodymyr Savchenko <contact@volodymyrsavchenko.com>
2021-05-08 10:17:39 +0200

    compute version

--- make.sh
+++ make.sh
@@ -24,4 +24,16 @@ function clone_all_latest() {
         clone_latest_component drupal7-db-for-astrooda drupal7-db-for-astrooda # i really hope none of the data there is private
 }
 
+
+function compute-version() {
+    for d in drupal7-for-astrooda/sites/all/modules/astrooda drupal7-for-astrooda drupal7-for-astrooda/sites/all/themes/bootstrap_astrooda .; do (
+        dn=$(basename $(realpath $d))
+        cd $d
+        git branch | awk 'NF>1 {printf "'$dn':\n branch: "$2"\n commit: "}'
+        git describe --abbrev=8 --always --tags
+    ) done > version.yaml
+
+    cat version.yaml | md5sum | cut -c 1-8
+}
+
 $@
